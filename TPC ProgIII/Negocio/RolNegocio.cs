2025b198701d5eb9bf528146834de9f365919f3a
Matias Ballesteros Quintana <matias.quintanaballesteros@gmail.com>
2025-11-13 17:30:14 -0300

    Create Negocio Usuario y Rol
    Creacion del ABM completo de usuario y rol

---
+++ TPC ProgIII/Negocio/RolNegocio.cs
@@ -0,0 +1,103 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+
+namespace Negocio
+{
+    public class RolNegocio
+    {
+        public List<Rol> Listar()
+        {
+            List<Rol> lista = new List<Rol>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT IdRol, NombreRol, Descripcion FROM Rol");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Rol aux = new Rol();
+                    aux.IdRol = (int)datos.Lector["IdRol"];
+                    aux.NombreRol = (string)datos.Lector["NombreRol"];
+
+                    if (datos.Lector["Descripcion"] != DBNull.Value)
+                        aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Agregar(Rol nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("INSERT INTO Rol (NombreRol, Descripcion) VALUES (@NombreRol, @Descripcion)");
+                datos.setearParametro("@NombreRol", nuevo.NombreRol);
+                datos.setearParametro("@Descripcion", (object)nuevo.Descripcion ?? DBNull.Value);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Modificar(Rol rol)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("UPDATE Rol SET NombreRol = @NombreRol, Descripcion = @Descripcion WHERE IdRol = @IdRol");
+                datos.setearParametro("@NombreRol", rol.NombreRol);
+                datos.setearParametro("@Descripcion", (object)rol.Descripcion ?? DBNull.Value);
+                datos.setearParametro("@IdRol", rol.IdRol);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("DELETE FROM Rol WHERE IdRol = @IdRol");
+                datos.setearParametro("@IdRol", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+             //Esto fallará si un Usuario está usando este Rol.
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ TPC ProgIII/Negocio/UsuarioNegocio.cs
@@ -0,0 +1,172 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+
+namespace Negocio
+{
+    public class UsuarioNegocio
+    {
+        public List<Usuario> Listar()
+        {
+            RolNegocio rolNegocio = new RolNegocio();
+            List<Rol> listaDeRoles = rolNegocio.Listar();
+            List<Usuario> listaDeUsuarios = new List<Usuario>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = @"
+            SELECT 
+                IdUsuario, Nombre, Apellido, Email, Telefono, 
+                Direccion, Localidad, FechaRegistro, Activo,
+                IdRol 
+            FROM 
+                Usuario";
+
+                datos.setearConsulta(consulta);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Usuario aux = new Usuario();
+                    aux.IdUsuario = (int)datos.Lector["IdUsuario"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Apellido = (string)datos.Lector["Apellido"];
+                    aux.Email = (string)datos.Lector["Email"];
+
+                    if (datos.Lector["Telefono"] != DBNull.Value)
+                        aux.Telefono = (string)datos.Lector["Telefono"];
+
+                    if (datos.Lector["Direccion"] != DBNull.Value)
+                        aux.Direccion = (string)datos.Lector["Direccion"];
+
+                    if (datos.Lector["Localidad"] != DBNull.Value)
+                        aux.Localidad = (string)datos.Lector["Localidad"];
+
+                    aux.FechaRegistro = (DateTime)datos.Lector["FechaRegistro"];
+                    aux.Activo = (bool)datos.Lector["Activo"];
+                    int idRolDelUsuario = (int)datos.Lector["IdRol"];
+                    aux.Rol = listaDeRoles.Find(r => r.IdRol == idRolDelUsuario);
+
+                    listaDeUsuarios.Add(aux);
+                }
+                return listaDeUsuarios;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void Agregar(Usuario nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                string consulta = @"
+                    INSERT INTO Usuario (Nombre, Apellido, Email, Telefono, Direccion, Localidad, IdRol) 
+                    VALUES (@Nombre, @Apellido, @Email, @Telefono, @Direccion, @Localidad, @IdRol)";
+
+                datos.setearConsulta(consulta);
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Apellido", nuevo.Apellido);
+                datos.setearParametro("@Email", nuevo.Email);
+                datos.setearParametro("@Telefono", (object)nuevo.Telefono ?? DBNull.Value);
+                datos.setearParametro("@Direccion", (object)nuevo.Direccion ?? DBNull.Value);
+                datos.setearParametro("@Localidad", (object)nuevo.Localidad ?? DBNull.Value);
+
+                datos.setearParametro("@IdRol", nuevo.Rol.IdRol);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Modificar(Usuario usuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                string consulta = @"
+                    UPDATE Usuario SET 
+                        Nombre = @Nombre, Apellido = @Apellido, Email = @Email, 
+                        Telefono = @Telefono, Direccion = @Direccion, 
+                        Localidad = @Localidad, IdRol = @IdRol, Activo = @Activo 
+                    WHERE 
+                        IdUsuario = @IdUsuario";
+
+                datos.setearConsulta(consulta);
+                datos.setearParametro("@Nombre", usuario.Nombre);
+                datos.setearParametro("@Apellido", usuario.Apellido);
+                datos.setearParametro("@Email", usuario.Email);
+                datos.setearParametro("@Telefono", (object)usuario.Telefono ?? DBNull.Value);
+                datos.setearParametro("@Direccion", (object)usuario.Direccion ?? DBNull.Value);
+                datos.setearParametro("@Localidad", (object)usuario.Localidad ?? DBNull.Value);
+                datos.setearParametro("@IdRol", usuario.Rol.IdRol);
+                datos.setearParametro("@Activo", usuario.Activo);
+                datos.setearParametro("@IdUsuario", usuario.IdUsuario);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        // Eliminación Física
+        public void EliminarFisico(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                // Esto fallará si el usuario tiene Pedidos o Carrito asociados, por las claves foráneas.
+                datos.setearConsulta("DELETE FROM Usuario WHERE IdUsuario = @IdUsuario");
+                datos.setearParametro("@IdUsuario", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        // Eliminación Lógica
+        public void EliminarLogico(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("UPDATE Usuario SET Activo = 0 WHERE IdUsuario = @IdUsuario");
+                datos.setearParametro("@IdUsuario", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
