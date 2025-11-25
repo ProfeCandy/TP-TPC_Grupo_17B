76218c27a91b1c0eb8c20f08f7f9575f3fa582ca
Matias Ballesteros Quintana <matias.quintanaballesteros@gmail.com>
2025-11-24 23:50:11 -0300

    Create PedidoExitoso.cs
    Ahora hay una logica detras pero simulada, si das a finalizar la compra te envia a la ventana de pedido exitoso y te muestra el carrito con lo comprado, el total gastado y el carrito pierde los items que tenia guardado, ademas ahora redirecciona al login y al historial de pedidos

--- TPC ProgIII/Frontend/PedidoExitoso.aspx.cs
+++ TPC ProgIII/Frontend/PedidoExitoso.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
 
 namespace Frontend
 {
@@ -11,7 +13,26 @@ namespace Frontend
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (Session["compraRealizada"] != null)
+                {
+                    List<CarritoItem> itemsComprados = (List<CarritoItem>)Session["compraRealizada"];
 
+                    repDetalleCompra.DataSource = itemsComprados;
+                    repDetalleCompra.DataBind();
+
+                    decimal total= itemsComprados.Sum(item => item.Producto.Precio * item.Cantidad);
+                    lblTotalPagado.Text = "$ " + total.ToString("N0");
+                 
+                    //Para recargar y que no falle
+                    Session["compraRealizada"] = null;
+                }
+                else
+                {
+                    Response.Redirect("~/Productos/Productos.aspx");
+                }
+            }
         }
     }
 }
